b14a9c1e73ccdac146926684acaa7a144bb9d59b
abdullah048 <abdullahshahbaz048@gmail.com>
2024-07-07 11:48:07 +0500

    nothing to serious

--- src/server.js
+++ src/server.js
@@ -2,22 +2,21 @@ const initializeClient = require('./utils/createClient');
 const hadithapi = require('./services/fetch-hadidth');
 
 const groups = [
-  // { id: '923207411866-1587564115@g.us', name: 'Brothers 🌹' },
+  { id: '923207411866-1587564115@g.us', name: 'Brothers 🌹' },
   { id: '923059663768-1537270210@g.us', name: 'CS Boys 2k18 ❤ NFC' },
   { id: '120363084043163851@g.us', name: 'BUSY LOGO KA GROUP 😂' },
-  // { id: '120363045810970768@g.us', name: 'No Pubg ✋' },
+  { id: '120363045810970768@g.us', name: 'No Pubg ✋' },
 ];
 
 const client = initializeClient();
 
 client.on('ready', async () => {
   console.log('Client is ready. Setting up the schedule.');
-
-  const message = await hadithapi();
-  console.log(message);
-  groups.forEach(({ id, name }) => {
-    client.sendMessage(id, message);
-  });
+  // const message = await hadithapi();
+  // console.log(message);
+  // groups.forEach(({ id, name }) => {
+  //   client.sendMessage(id, message);
+  // });
 });
 
 // Schedule task to run every Monday to Friday at 10 AM
